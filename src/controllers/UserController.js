708c1b0ef828b516690f09d37f98ef86f36be9e4
João Costa <costav1402@gmail.com>
2022-02-12 12:21:50 -0300

    show user controller

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -26,5 +26,26 @@ const UserController = {
       });
     }
   },
+  show: async (req, res) => {
+    try {
+      const user = await UserModel.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(404).json({
+          errors: ['Usuário não encontrado'],
+        });
+      }
+      const {
+        id, name, email, isAdmin,
+      } = user;
+      return res.json({
+        id, name, email, isAdmin,
+      });
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  },
 };
 export default UserController;
--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -4,4 +4,5 @@ import UserController from '../controllers/UserController';
 const route = express.Router();
 route.post('/', UserController.store);
 route.get('/', UserController.index);
+route.get('/:id', UserController.show);
 export default route;
